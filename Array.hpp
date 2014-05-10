1626bf1f6652445d63d74a6f5cb9701a894d386c
Yi Wang <yige@Yi.local>
2014-05-10 17:20:17 -0400

    need test

--- Array.hpp
+++ Array.hpp
@@ -27,49 +27,12 @@ struct WrongTemplate: public std::runtime_error
     {}
 };
 void throwOutOfRangeException(){
-    throw OutOfRange("Out of Range");
+    throw OutOfRange("Out of Range Exception");
 }
 void throwWrongTemplateException(){
-    throw WrongTemplate("Wrong Template");
+    throw WrongTemplate("Wrong Template Exception");
 }
-/*******************/
-template<typename T>
-T *ArrayOneDimension(size_t size){
-    return new T[size];
-}
-size_t getLength(){
-    return 1;
-}
-size_t getLength(int args, ...){
-    int i;
-    va_list vl;
-    va_start(vl,args);
-    size_t len = 1;
-    typedef std::integral_constant<size_t, 0> zero_t;
-    for(i=0; i< args; i++) {
-        size_t val = va_arg(vl, size_t);
-        // size_t ss = val;
-        //typedef std::integral_constant<size_t, std::move(ss)> val_t;
-        // static_assert(val_t::value > zero_t::value, "dim should be positive!");
-        len = len*val;
-    }
-    va_end(vl);
-    return len;
-}
-size_t* getDim(int args, ...){
-    int i;
-    va_list vl;
-    va_start(vl,args);
-    size_t* dim = new size_t[args];
-    for(i=0; i< args; i++) {
-        size_t val = va_arg(vl, size_t);
-        // static_assert(val> 0, "dim should be positive!");
-        dim[i] = val;
-    }
-    va_end(vl);
-    return dim;
-}
-/*******************/
+
 template<typename T, size_t D, size_t... Dims>
 class Array{
 public:
@@ -80,7 +43,7 @@ public:
     
 public:
     friend class Array<T , Dims...>;
-    Array(): ValueType(typeid(T)), dimSize(sizeof...(Dims)), size(D){
+    Array(): ValueType(typeid(T)), arr(nullptr), dimSize(sizeof...(Dims)), size(D){
         if(debug){
             std::cout<<"allocate array\n";
         }
@@ -109,7 +72,16 @@ public:
         }
     }
     Array &operator=(const Array &array){
+        if(debug){
+            std::cout<<"copy assignment 1\n";
+        }
         size_t i;
+        if(array.arr == arr){
+            if(debug){
+                std::cout<<"    self assignment 1: no op\n";
+            }
+            return *this;
+        }
         if(dimSize != array.dimSize || size != array.size){
             throwWrongTemplateException();
         }
@@ -120,19 +92,28 @@ public:
     }
     template <typename U>
     Array &operator=(const Array<U, D, Dims...> & array){
-        if(arr == array.arr){
-            return *this;
+        if(debug){
+            std::cout<<"copy assignment 2: different type\n";
         }
         size_t i;
         for(i=0; i< size; i++){
             arr[i] = array.arr[i];
         }
+        return *this;
     }
-    Array<T, Dims... >& operator[](const size_t d1) const{
-        //std::cout << "dd: "<<typeid(arr).name() << std::endl;
+    Array<T, Dims... >& operator[](const size_t d1) {
         if(debug){
-            std::cout<<"call array[] at Array. ";
+            std::cout<<"call array["<<d1<<"] at Array. ";
             std::cout<<"Dimension of Array is "<<dimSize<<"\n";
+        }
+        if(d1>=size){
+            throwOutOfRangeException();
+        }
+        return arr[d1];
+    }
+    const Array<T, Dims... >& operator[](const size_t d1) const{
+        if(d1>=size){
+            throwOutOfRangeException();
         }
         return arr[d1];
     }
@@ -154,7 +135,7 @@ public:
             , nextIterator(fmi.nextIterator)
             , size(fmi.size)
             , curdims(fmi.curdims)
-            , isEnd(false){ }
+            , isEnd(fmi.isEnd){ }
         FirstDimensionMajorIterator &
         operator=(const FirstDimensionMajorIterator & fmi){
             arr = fmi.arr;
@@ -178,7 +159,10 @@ public:
             if(fmi1.isEnd != fmi2.isEnd){
                 return false;
             }
-            return fmi1.nextIterator == fmi1.nextIterator;
+            if(fmi1.isEnd){
+                return true;
+            }
+            return fmi1.nextIterator == fmi2.nextIterator;
         }
         friend bool operator!=(const FirstDimensionMajorIterator & fmi1,
                         const FirstDimensionMajorIterator & fmi2){
@@ -194,18 +178,36 @@ public:
             if(fmi1.isEnd != fmi2.isEnd){
                 return true;
             }
-            return fmi1.nextIterator != fmi1.nextIterator;
+            if(fmi1.isEnd){
+                return false;
+            }
+            return fmi1.nextIterator != fmi2.nextIterator;
+        }
+        void relocate(){
+            nextIterator.arr = &(arr->arr[curdims]);
+            nextIterator.isEnd = false;
+            nextIterator.relocate();
         }
         FirstDimensionMajorIterator &operator++(){
+            if(debug){
+                std::cout << "fmi pre increment\n";
+            }
             ++nextIterator;
-            if(nextIterator.curdims == 0){
+            if(nextIterator.isEnd && nextIterator.curdims == 0){
+                if(debug){
+                    std::cout << "next dimension\n";
+                }
                 curdims = curdims+1;
                 if(curdims == size){
-                    curdims = curdims%size;
+                    curdims = 0;
                     isEnd = true;
                 }
-                nextIterator = arr->arr[curdims].fmbegin();
+                else{
+                    isEnd = false;
+                }
+                relocate();
             }
+            
             return *this;
         }
         FirstDimensionMajorIterator operator++(int){
@@ -216,7 +218,106 @@ public:
         T &operator*() const{
             return *nextIterator;
         }
+        void printDim(){
+            std::cout << curdims;
+            if(isEnd){
+                std::cout<<"T ";
+            }
+            else
+                std::cout<<"F ";
+            nextIterator.printDim();
+        }
     };
+    class LastDimensionMajorIterator{
+    public:
+        friend class Array<T, Dims...>;
+        friend class Array<T, Dims...>::LastDimensionMajorIterator;
+        Array<T, D, Dims...> *arr;
+        typename Array<T, Dims...>::LastDimensionMajorIterator nextIterator;
+        size_t size;
+        size_t curdims;
+        bool isEnd;
+        LastDimensionMajorIterator():size(D),curdims(0), isEnd(false) {}
+        LastDimensionMajorIterator(const LastDimensionMajorIterator & fmi): arr(fmi.arr)
+        , nextIterator(fmi.nextIterator)
+        , size(fmi.size)
+        , curdims(fmi.curdims)
+        , isEnd(fmi.curdims){ }
+        LastDimensionMajorIterator &
+        operator=(const LastDimensionMajorIterator & fmi){
+            arr = fmi.arr;
+            size = fmi.size;
+            curdims = fmi.curdims;
+            nextIterator = fmi.nextIterator;
+            isEnd = fmi.isEnd;
+            return *this;
+        }
+        friend bool operator==(const LastDimensionMajorIterator & fmi1,
+                               const LastDimensionMajorIterator &fmi2){
+            if(fmi1.arr!=fmi2.arr){
+                return false;
+            }
+            if(fmi1.size!=fmi2.size){
+                return false;
+            }
+            if(fmi1.curdims!=fmi2.curdims){
+                return false;
+            }
+            if(fmi1.isEnd != fmi2.isEnd){
+                return false;
+            }
+            return fmi1.nextIterator == fmi2.nextIterator;
+        }
+        friend bool operator!=(const LastDimensionMajorIterator & fmi1,
+                               const LastDimensionMajorIterator & fmi2){
+            if(fmi1.arr!=fmi2.arr){
+                return true;
+            }
+            if(fmi1.size!=fmi2.size){
+                return true;
+            }
+            if(fmi1.curdims!=fmi2.curdims){
+                return true;
+            }
+            if(fmi1.isEnd != fmi2.isEnd){
+                return true;
+            }
+            return fmi1.nextIterator != fmi2.nextIterator;
+        }
+        void relocate(){
+            nextIterator.arr = &(arr->arr[curdims]);
+            nextIterator.relocate();
+            isEnd = nextIterator.isEnd;
+        }
+        LastDimensionMajorIterator &operator++(){
+            curdims = (curdims+1)%size;
+            if(curdims == 0){
+                isEnd = true;
+                ++nextIterator;
+            }
+            relocate();
+            return *this;
+        }
+        LastDimensionMajorIterator operator++(int){
+            LastDimensionMajorIterator tmp = *this;
+            ++(*this);
+            return tmp;
+        }
+        T &operator*() const{
+            return *nextIterator;
+        }
+        void printDim(){
+            std::cout << curdims;
+            if(isEnd){
+                std::cout<<"T ";
+            }
+            else
+                std::cout<<"F ";
+            nextIterator.printDim();
+        }
+    
+    };
+    
     FirstDimensionMajorIterator fmbegin(){
         FirstDimensionMajorIterator fmi;
         fmi.arr = this;
@@ -226,10 +327,24 @@ public:
     FirstDimensionMajorIterator fmend(){
         FirstDimensionMajorIterator fmi;
         fmi.arr = this;
-        fmi.nextIterator = arr[0].fmbegin();
         fmi.isEnd = true;
+        fmi.nextIterator = arr[0].fmend();
         return fmi;
     }
+    LastDimensionMajorIterator lmbegin(){
+        LastDimensionMajorIterator fmi;
+        fmi.arr = this;
+        fmi.nextIterator = arr[0].lmbegin();
+        return fmi;
+    }
+    LastDimensionMajorIterator lmend(){
+        LastDimensionMajorIterator fmi;
+        fmi.arr = this;
+        fmi.isEnd = true;
+        fmi.nextIterator = arr[0].lmend();
+        return fmi;
+    }
+
 };
 template<typename T, size_t D>
 class Array<T, D>{
@@ -240,7 +355,7 @@ public:
     size_t size;
     
 public:
-    Array(): ValueType(typeid(T)), dimSize(0), size(D){
+    Array(): ValueType(typeid(T)),arr(nullptr), dimSize(0), size(D){
         if(debug){
             std::cout<<"allocate array ["<<dimSize<<"]\n";
         }
@@ -280,24 +395,30 @@ public:
     }
     template <typename U>
     Array &operator=(const Array<U, D> & array){
-        if(arr == array.arr){
-            return *this;
-        }
         size_t i;
         for(i=0; i< size; i++){
             arr[i] = array.arr[i];
         }
+        return *this;
     }
-    T& operator[](const size_t d1) const{
-        //std::cout << "dd: "<<typeid(arr).name() << std::endl;
+    T& operator[](const size_t d1){
         if(debug){
             std::cout<<"call array[] at Array. ";
             std::cout<<"Dimension of Array is "<<dimSize<<"\n";
         }
+        if(d1>=size){
+            throwOutOfRangeException();
+        }
+        return arr[d1];
+    }
+    const T &operator[](const size_t d1) const{
+        if(d1>=size){
+            throwOutOfRangeException();
+        }
         return arr[d1];
     }
     ~Array(){
-        delete arr;
+        delete []arr;
     }
     class FirstDimensionMajorIterator{
     public:
@@ -335,7 +456,10 @@ public:
             if(fmi1.isEnd != fmi2.isEnd){
                 return false;
             }
-            return fmi1.elem == fmi1.elem;
+            if(fmi1.isEnd){
+                return true;
+            }
+            return fmi1.elem == fmi2.elem;
         }
         friend bool operator!=(const FirstDimensionMajorIterator & fmi1,
                                const FirstDimensionMajorIterator & fmi2){
@@ -351,15 +475,24 @@ public:
             if(fmi1.isEnd != fmi2.isEnd){
                 return true;
             }
-            return fmi1.elem != fmi1.elem;
+            if(fmi1.isEnd){
+                return false;
+            }
+            return fmi1.elem != fmi2.elem;
+        }
+        void relocate(){
+            elem = &(arr->arr[curdims]);
         }
         FirstDimensionMajorIterator &operator++(){
-            curdims = curdims+1;
+            curdims ++;
             if(curdims == size){
-                curdims = curdims%size;
+                curdims = 0;
                 isEnd = true;
             }
-            elem = &(arr->arr[curdims]);
+            else{
+                isEnd = false;
+            }
+            relocate();
             return *this;
         }
         FirstDimensionMajorIterator operator++(int){
@@ -370,19 +503,27 @@ public:
         T &operator*() const{
             return *elem;
         }
-
+        void printDim(){
+            std::cout << curdims;
+            if(isEnd){
+                std::cout<<"T ";
+            }
+            else
+                std::cout<<"F ";
+        }
     };
     class LastDimensionMajorIterator{
     public:
+        friend class Array<T, D>;
         Array<T, D> *arr;
         T* elem;
         size_t size;
         size_t curdims;
         bool isEnd;
-        LastDimensionMajorIterator():size(D),curdims(size-1), isEnd(false) { }
+        LastDimensionMajorIterator():size(D),curdims(0), isEnd(false) { }
         LastDimensionMajorIterator(const LastDimensionMajorIterator & lmi): arr(lmi.arr)
         , elem(lmi.elem)
-        , size(lmi.size)
+        , size(0)
         , curdims(lmi.curdims)
         , isEnd(lmi.isEnd){ }
         LastDimensionMajorIterator &
@@ -394,6 +535,7 @@ public:
             isEnd = lmi.isEnd;
             return *this;
         }
+        
         friend bool operator==(const LastDimensionMajorIterator & fmi1,
                                const LastDimensionMajorIterator &fmi2){
             if(fmi1.arr!=fmi2.arr){
@@ -408,7 +550,10 @@ public:
             if(fmi1.isEnd != fmi2.isEnd){
                 return false;
             }
-            return fmi1.elem == fmi1.elem;
+            if(fmi1.isEnd){
+                return true;
+            }
+            return fmi1.elem == fmi2.elem;
         }
         friend bool operator!=(const LastDimensionMajorIterator & fmi1,
                                const LastDimensionMajorIterator & fmi2){
@@ -424,7 +569,13 @@ public:
             if(fmi1.isEnd != fmi2.isEnd){
                 return true;
             }
-            return fmi1.elem != fmi1.elem;
+            if(fmi1.isEnd){
+                return false;
+            }
+            return fmi1.elem != fmi2.elem;
+        }
+        void relocate(){
+            elem = &(arr->arr[curdims]);
         }
         LastDimensionMajorIterator &operator++(){
             curdims = curdims+1;
@@ -436,24 +587,46 @@ public:
             return *this;
         }
         LastDimensionMajorIterator operator++(int){
-            FirstDimensionMajorIterator tmp = *this;
+            LastDimensionMajorIterator tmp = *this;
             ++(*this);
             return tmp;
         }
         T &operator*() const{
             return *elem;
         }
+        void printDim(){
+            std::cout << curdims;
+            if(isEnd){
+                std::cout<<"T ";
+            }
+            else
+                std::cout<<"F ";
+        }
     };
     FirstDimensionMajorIterator fmbegin(){
         FirstDimensionMajorIterator fmi;
         fmi.arr = this;
         fmi.elem = &arr[0];
+        fmi.isEnd = false;
         return fmi;
     }
     FirstDimensionMajorIterator fmend(){
         FirstDimensionMajorIterator fmi;
         fmi.arr = this;
-        fmi.elem = arr[0];
+        fmi.elem = &arr[0];
+        fmi.isEnd = true;
+        return fmi;
+    }
+    LastDimensionMajorIterator lmbegin(){
+        LastDimensionMajorIterator fmi;
+        fmi.arr = this;
+        fmi.elem = &arr[0];
+        return fmi;
+    }
+    LastDimensionMajorIterator lmend(){
+        LastDimensionMajorIterator fmi;
+        fmi.arr = this;
+        fmi.elem = &arr[0];
         fmi.isEnd = true;
         return fmi;
     }
--- test.cpp
+++ test.cpp
@@ -2,10 +2,28 @@
 #include <string>
 #include <exception>
 #include <typeinfo>
+#include <assert.h>
 #include "Array.hpp"
 using namespace std;
-
-int main(){
+void test0(){
+    cs540::Array<int, 10> a;
+    int i;
+    for(i=0; i<10; i++){
+        a[i] = i;
+    }
+    i=0;
+    for (auto it = a.fmbegin(); it != a.fmend(); ++it) {
+        assert(*it == i);
+        i++;
+    }
+    i=0;
+    for (auto it = a.lmbegin(); it != a.lmend(); ++it) {
+        assert(*it == i);
+        i++;
+    }
+}
+void test1() {
+    cout << "test 1: "<< endl;
     //size_t i;
     cs540::Array<int, 2, 3> a;
     cout << endl;
@@ -25,17 +43,17 @@ int main(){
     a[0][0] = 100;
     a[0][1] = 123;
     /*
-    auto iter = a.fmbegin();
-    
-    cout << *iter<< endl;
-    *iter = 111;
-    cout << a[0][0]<< endl;
-    ++iter;
-    cout << *iter<<" "<<a[0][1] << endl;
-    
-    auto iter1 = a.fmbegin();
-    cout << *(iter1++) << endl;
-    cout << *iter1 << endl;
+     auto iter = a.fmbegin();
+     
+     cout << *iter<< endl;
+     *iter = 111;
+     cout << a[0][0]<< endl;
+     ++iter;
+     cout << *iter<<" "<<a[0][1] << endl;
+     
+     auto iter1 = a.fmbegin();
+     cout << *(iter1++) << endl;
+     cout << *iter1 << endl;
      */
     auto iter1 = a.fmbegin();
     auto iter2 = a.fmend();
@@ -43,5 +61,116 @@ int main(){
     for(auto iter3=a.fmbegin(); iter3!=a.fmend(); iter3++){
         cout << *iter3 <<" ";
     }
+    cout << endl;
+    for(auto iter3=a.lmbegin(); iter3!=a.lmend(); iter3++){
+        cout << *iter3 <<" ";
+    }
+    cout << endl;
+}
+void test2(){
+    cout << "test 2: "<< endl;
+    // Define a 2X3X4 array of integers.  Elements are uninitialized.
+    cs540::Array<int, 2, 3, 4> a, b;
+    cs540::Array<short, 2, 3, 4> c;
+    //cs540::Array<int, 0> e1; // This line must cause a compile-time error.
+    
+    // Range-checked indexing.
+    a[0][0][0] = 1234;
+    a[0][0][3] = 3;
+    a[1][1][1] = a[0][0][0];
+    a[1][0][3] = 103;
+    a[0][2][3] = 5678; // Set the element in the first plane, 3rd row, and 4th column.
+    a[1][1][3] = 113;
+    a[1][2][1] = 121;
+    a[1][0][0] = 110;
+    a[1][0][1] = 101;
+    a[0][0][1] = 1;
+    /*
+     try{
+     a[0][3][0] = 1; // Out of range, throws.
+     }
+     catch(exception& e){
+     cout << "meet exception: "<< e.what()<<endl;
+     }*/
+    
+    a = a; // Self-assignment must be a no-op.
+    b = a; // Okay, as long as dimensions and type match exactly.
+    a = c; // Member template constructor.
+    //cout << "~~~here2"<<endl;
+    
+    const cs540::Array<int, 2, 3, 4> &const_ref(a);
+    
+    int i = const_ref[0][0][0]; // Okay.
+    i++;
+    //const_ref[0][0][0] = 1; // Syntax error.
+    
+    // Iterate through in plane major order.  (Note: Normally, you would use
+    // auto to simplify.)
+    
+    for (auto it = b.fmbegin(); it != b.fmend(); ++it) {
+        cout << *it << " <";
+        it.printDim();
+        cout <<">\n";
+        i++;
+        if(i>25)
+            break;
+    }
+    cout << endl;
+    // Iterate through in column major order.
+    for (auto it = b.lmbegin(); it != b.lmend(); ++it) {
+        cout << *it << " ";
+    }
+    cout << endl;
+}
+void test3(){
+    cs540::Array<int, 2, 3, 2> a, b;
+    a[0][0][0] = 0;
+    a[0][0][1] = 1;
+    a[0][1][0] = 2;
+    a[0][1][1] = 3;
+    a[0][2][0] = 4;
+    a[0][2][1] = 5;
+    a[1][0][0] = 6;
+    a[1][0][1] = 7;
+    a[1][1][0] = 8;
+    a[1][1][1] = 9;
+    a[1][2][0] = 10;
+    a[1][2][1] = 11;
+    int i=0;
+    for(auto it = a.fmbegin(); it != a.fmend(); ++it){
+        assert(*it == i);
+        /*cout << *it << " ";
+        it.printDim();
+        cout << "\n";*/
+        i++;
+         
+    }
+    b[0][0][0] = 0;
+    b[1][0][0] = 1;
+    b[0][1][0] = 2;
+    b[1][1][0] = 3;
+    b[0][2][0] = 4;
+    b[1][2][0] = 5;
+    b[0][0][1] = 6;
+    b[1][0][1] = 7;
+    b[0][1][1] = 8;
+    b[1][1][1] = 9;
+    b[0][2][1] = 10;
+    b[1][2][1] = 11;
+    i=0;
+    for(auto it = b.lmbegin(); it != b.lmend(); ++it){
+        assert(*it == i);
+       /* cout << *it << " ";
+         it.printDim();
+         cout << "\n";*/
+        i++;
+    }
+    
+}
+int main(){
+    test0();
+    test1();
+    test2();
+    test3();
     return 1;
 }
